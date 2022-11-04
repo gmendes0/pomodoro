059d2de6dc5f0278c3a7a95de34491a6a30c5226
gxbriel9g <gmendes230@gmail.com>
2022-11-04 17:15:35 -0300

    feat: add useReducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useMemo, useState } from 'react'
+import { createContext, ReactNode, useMemo, useReducer, useState } from 'react'
 
 interface Cycle {
   id: string
@@ -31,15 +31,92 @@ interface CycleProviderProps {
   children: ReactNode
 }
 
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
 /**
  * Evitar acoplar bibliotecas externas no context para caso a gente troque a lib, o context continue funcionando
  */
 export function CycleProvider(props: CycleProviderProps) {
   /**
+   * Assim como o useState, serve para armazenar e alterar uma informação
+   * É interessante utilizar o useReducer para armazenar
+   * informações mais complexas com alterações complexas e com alteraçoes provindas de diferentes lugares da aplicaçao
+   * É bom para centralizar todas as alterações que podem acontecer dentro de um estado, ex: criar, alterar, etc...
+   *
+   * 1 param = uma funçao com 2 params
+   *           o primeiro é o valor atual em tempo real do estado
+   *           o segundo é uma action, ou seja, qual ação queremos realizar para alterar o estado
+   *           A função precisa retornar um valor, que será armazenado no estado
+   * 2 param = valor inicial do estado
+   *
+   * O useReducer retorna um array bem parecido com o useState, onde o primeiro valor é o estado e o segundo
+   * uma função para disparar uma action
+   *
+   * No caso do createNewCycle por exemplo, era necessário chamar um setCycles para adicionar um novo ciclo ao estado
+   * e logo em seguida chamar um setActiveCycleId passando o id do novo ciclo adicionado ao estado, ou seja,
+   * precisavamos atualizar 2 estados de uma vez só. Com o useReducer, podemos criar um estado que contenha essas 2
+   * infos e quando dispararmos a action 'ADD_NEW_CYCLE', atualizamos a lista de ciclos e o activeCycleId ao mesmo tempo
+   * sem precisarmos chamar 2 funçoes diferentes
+   */
+  const [cyclesState, dispatch] = useReducer(
+    (state: CyclesState, action: any) => {
+      // Essa função é executada quando o dispatch é chamado
+      // O valor passado no parametro do dispatch() vai estar disponível no 'action'
+
+      // console.log(state)
+      // console.log(action)
+
+      switch (action.type) {
+        case 'ADD_NEW_CYCLE':
+          return {
+            ...state,
+            cycles: [...state.cycles, action.payload.newCycle],
+            activeCycleId: action.payload.newCycle.id,
+          }
+
+        case 'INTERRUPT_CURRENT_CYCLE':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId)
+                return { ...cycle, interruptedDate: new Date() }
+
+              return cycle
+            }),
+            activeCycleId: null,
+          }
+
+        case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+          return {
+            ...state,
+            cycles: state.cycles.map((cycle) => {
+              if (cycle.id === state.activeCycleId)
+                return { ...cycle, finishedDate: new Date() }
+
+              return cycle
+            }),
+          }
+
+        default:
+          return state
+      }
+    },
+    { cycles: [], activeCycleId: null }
+  )
+
+  /**
+   * Também da para desestruturar acima direto do array
+   */
+  const { activeCycleId, cycles } = cyclesState
+
+  /**
    * Sempre iniciar o estado com um valor
    */
-  const [cycles, setCycles] = useState<Cycle[]>([])
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+  // const [cycles, setCycles] = useState<Cycle[]>([])
+  // const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
   /**
@@ -51,16 +128,22 @@ export function CycleProvider(props: CycleProviderProps) {
   )
 
   function markCurrentCycleAsFinished() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId)
-          return { ...cycle, finishedDate: new Date() }
+    dispatch({
+      type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+      payload: {
+        activeCycleId,
+      },
+    })
 
-        return cycle
-      })
-    )
+    // setCycles((state) =>
+    //   state.map((cycle) => {
+    //     if (cycle.id === activeCycleId)
+    //       return { ...cycle, finishedDate: new Date() }
 
-    setActiveCycleId(null)
+    //     return cycle
+    //   })
+    // )
+    // setActiveCycleId(null)
   }
 
   function setSecondsPassed(seconds: number) {
@@ -75,21 +158,42 @@ export function CycleProvider(props: CycleProviderProps) {
       startDate: new Date(),
     }
 
-    setCycles((state) => [...state, newCycle])
-    setActiveCycleId(newCycle.id)
+    /**
+     * É preciso enviar alguma informaçao que possibilite distinguir dentro da função do useReducer
+     * qual ação deve ser feita.
+     *
+     * Normalmente, é utilizado o padrao {type: '', payload: {}}
+     */
+    dispatch({
+      type: 'ADD_NEW_CYCLE',
+      payload: {
+        newCycle,
+      },
+    })
+
+    // setCycles((state) => [...state, newCycle])
+    // setActiveCycleId(newCycle.id)
+
     setAmountSecondsPassed(0)
   }
 
   function interruptCurrentCycle() {
-    setCycles((state) =>
-      state.map((cycle) => {
-        if (cycle.id === activeCycleId)
-          return { ...cycle, interruptedDate: new Date() }
+    dispatch({
+      type: 'INTERRUPT_CURRENT_CYCLE',
+      payload: {
+        activeCycleId,
+      },
+    })
 
-        return cycle
-      })
-    )
-    setActiveCycleId(null)
+    // setCycles((state) =>
+    //   state.map((cycle) => {
+    //     if (cycle.id === activeCycleId)
+    //       return { ...cycle, interruptedDate: new Date() }
+
+    //     return cycle
+    //   })
+    // )
+    // setActiveCycleId(null)
   }
 
   return (
